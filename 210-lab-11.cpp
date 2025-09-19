9e016b02b659484ba8826580fcee5541edda7a8b
Krispy King <kking620@gmail.com>
2025-09-19 14:59:17 -0700

    Third commit

--- 210-lab-11.cpp
+++ 210-lab-11.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
-struct soccerFan
+const int NR_ROUNDS = 5, NR_BOWLERS = 5;
+
+struct bowlingScore
 {
-    string homeCountry;
-    string favoriteTeam;
-    int nGamesAttended;
+    string homeTown;
+    string localBowlingAlley;
+    int  * pinsKnockedDown;
 };
 
-void inputSoccerFan(soccerFan *);
-void displaySoccerFan(soccerFan *);
+void inputBowlingScore(bowlingScore *);
+void displayBowlingScore(bowlingScore *);
 
 int main()
 {
+    bowlingScore *bowlingHistory = new bowlingScore[NR_ROUNDS];
 
+    for (int i = 0; i < NR_BOWLERS; i++)
+        inputBowlingScore(&bowlingHistory[i]);
+
+    for (int i = 0; i < NR_BOWLERS; i++)
+        displayBowlingScore(&bowlingHistory[i]);
+
+    return 0;
 }
